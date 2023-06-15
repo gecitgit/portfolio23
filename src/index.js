e8e2310466d2f057ab371c94bd7c321b773b8419
+ <+>
2023-06-15 05:21:03 -0400

    Updated to include construction page while layout is being finalized

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+// import ReactDOM from 'react-dom';
 // import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './components/App';
+// import './index.css';
+import './construction.css';
+// import App from './components/App';
+import UnderConstruction from './components/UnderConstruction';
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<UnderConstruction />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+    <React.StrictMode>
+        <UnderConstruction />
+    </React.StrictMode>
+);
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
