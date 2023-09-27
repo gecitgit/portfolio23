9057bc83a03a7f86c847bfb1ddbef92cf3e89d37
+ <+>
2023-09-27 17:01:51 -0400

    Tweaked some CSS and cleaned up the overall folder

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,21 +5,20 @@ import ContactForm from './contactform';
 function Contact() {
     return (
         <div className="contact-info" id="contact">
-            <div>
                 <h1>Let's Build Together!</h1>
                 <div>
-                    <a href="https://www.linkedin.com/in/taulantismailgeci/" target="_blank" rel="noopener noreferrer" className="social-icons" aria-label="Click to visit my LinkedIn profile.">
-                        <TbBrandLinkedin size={125} />
-                    </a>
-                    <a href="https://www.github.com/gecitgit" target="_blank" rel="noopener noreferrer" className="social-icons" aria-label="Click to visit my GitHub profile.">
-                        <TbBrandGithub size={125} />
-                    </a>
+
+                <a href="https://www.linkedin.com/in/taulantismailgeci/" target="_blank" rel="noopener noreferrer" className="social-icons" aria-label="Click to visit my LinkedIn profile.">
+                    <TbBrandLinkedin size={125} />
+                </a>
+                <a href="https://www.github.com/gecitgit" target="_blank" rel="noopener noreferrer" className="social-icons" aria-label="Click to visit my GitHub profile.">
+                    <TbBrandGithub size={125} />
+                </a>
                 </div>
                 <p>Feel free to explore my projects and professional journey.</p>
-                <p style={{ fontWeight: "bolder", fontSize: "1.2rem"}}>Drop Me a Message:</p>
+                <p className="call-to-action">Drop Me a Message:</p>
                 <p>Whether you have a question, project idea, or just want to say hi, I'd love to hear from you!</p>
                 <ContactForm />
-            </div>
         </div>
     )
 }
